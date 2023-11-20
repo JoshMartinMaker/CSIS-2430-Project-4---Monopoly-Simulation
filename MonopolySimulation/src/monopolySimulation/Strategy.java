c139256b150a1d15fb08d9f1c98fd99dc1198815
Josh Martin <joshmartinmaker@gmail.com>
2023-11-20 16:53:15 -0700

    Add Strategy enum

---
+++ MonopolySimulation/src/monopolySimulation/Strategy.java
@@ -0,0 +1,12 @@
+package monopolySimulation;
+
+/**
+ * Represents a strategy for rolling when in jail in a game of Monopoly.
+ * 
+ * @author Josh Martin
+ *
+ */
+public enum Strategy {
+	A,
+	B
+}
