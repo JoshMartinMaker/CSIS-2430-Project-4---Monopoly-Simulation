13c3fcd5c800cc9e5ea9a79898b6b6833a974de3
Cristian Camargo <cluelessdreamxr@gmail.com>
2023-11-20 20:31:13 -0700

    Add GameSimulationClient.java

---
+++ MonopolySimulation/src/monopolySimulation/GameSimulationClient.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.RedBlackBST;
+
+public class GameSimulationClient {
+    private RedBlackBST<Integer, Integer[]> strategyAResults;
+    private RedBlackBST<Integer, Integer[]> strategyBResults;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void simulateGame(int maxTurns, String startegy){
+
+    }
+    private void writeResults(){
+
+    }
+}
