102c6912f09cbab301a90c950458c8d5bbae8bf1
Zach Royer <zach.royer14@gmail.com>
2023-11-20 18:40:33 -0700

    Created chanceCards enum

--- MonopolySimulation/src/Monopoly/GameSpace.java
+++ MonopolySimulation/src/Monopoly/GameSpace.java
@@ -8,28 +8,29 @@ package Monopoly;
  */
 public class GameSpace implements SpaceEffect {
 
-	private SpaceNames name;
+	private spaceNames name;
 	private int timeLandedOn;
 	private double percentLandedOn;
 	private static int maxTurns;
-	
-	public GameSpace(SpaceNames name, int maxTurns) {
+
+	public GameSpace(spaceNames name, int maxTurns) {
 		this.name = name;
 		this.maxTurns = maxTurns;
 	}
+
 	@Override
 	public spaceEffects effect() {
 		return null;
 	}
 
-	public SpaceNames getName() {
+	public spaceNames getName() {
 		return this.name;
 	}
-	
+
 	public int getTimesLandedOn() {
 		return this.timeLandedOn;
 	}
-	
+
 	public double getPercentLandedOn() {
 		return this.percentLandedOn;
 	}
