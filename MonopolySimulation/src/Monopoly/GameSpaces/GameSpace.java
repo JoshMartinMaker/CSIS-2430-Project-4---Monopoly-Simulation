9285c3f86e43e1b3419bc0116a12f49427fbdefd
Josh Martin <joshmartinmaker@gmail.com>
2023-12-03 21:33:51 -0700

    Finish implementing GameSpace class, add incrementTimesLandedOn method, and add doc comments

--- MonopolySimulation/src/Monopoly/GameSpaces/GameSpace.java
+++ MonopolySimulation/src/Monopoly/GameSpaces/GameSpace.java
@@ -1,39 +1,75 @@
-/**
- * 
- */
 package Monopoly.GameSpaces;
 
 import monopolySimulation.SpaceNames;
 
 /**
+ * Represents a space on a Monopoly board.
+ * 
  * @author Zach Royer
+ * @author Josh Martin
  */
 public class GameSpace implements SpaceEffect {
 
-	private SpaceNames name;
-	private int timeLandedOn;
-	private double percentLandedOn;
-	private static int maxTurns;
+	private final SpaceNames name;
+	private int timesLandedOn = 0;
+	private double percentLandedOn = 0;
+	private final int maxTurns;
 
+	/**
+	 * Cosntructor for GameSpace class.
+	 * 
+	 * @param name The name of this space.
+	 * @param maxTurns The number of turns to base {@code percentLandedOn} on.
+	 */
 	public GameSpace(SpaceNames name, int maxTurns) {
 		this.name = name;
 		this.maxTurns = maxTurns;
 	}
 
+	/**
+	 * Returns the effect when landing on this space.
+	 * 
+	 * @return The effect when landing on this space.
+	 */
 	@Override
 	public SpaceEffects effect() {
 		return null;
 	}
 
+	/**
+	 * Returns the name of this space.
+	 * 
+	 * @return The name of this space.
+	 */
 	public SpaceNames getName() {
 		return this.name;
 	}
 
+	/**
+	 * Returns the number of times this space has been landed on.
+	 * 
+	 * @return The number of times this space has been landed on.
+	 */
 	public int getTimesLandedOn() {
-		return this.timeLandedOn;
+		return this.timesLandedOn;
 	}
 
+	/**
+	 * Returns the percentage of times this space has been landed on over an entire game.
+	 * 
+	 * @return The percentage of times this space has been landed on over an entire game.
+	 */
 	public double getPercentLandedOn() {
+		
+		this.percentLandedOn = this.timesLandedOn / this.maxTurns;
+		
 		return this.percentLandedOn;
+	}
+	
+	/**
+	 * Increments the number of times this space has been landed on.
+	 */
+	public void incrementTimesLandedOn() {
+		this.timesLandedOn++;
 	}
 }
